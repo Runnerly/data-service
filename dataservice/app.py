36d832847b48c81bd7d2363d88a27b2e3db0849f
Tarek Ziade <tarek@ziade.org>
2017-03-08 23:23:33 +0100

    plugged jwt auth

--- dataservice/app.py
+++ dataservice/app.py
@@ -1,9 +1,57 @@
+import os
+from werkzeug.exceptions import HTTPException
 from flakon import create_app
-from dataservice.views import blueprints
-from dataservice.database import db
+from flakon.util import error_handling
+from flask import request, abort
+
+import jwt
+
+from .views import blueprints
+from .database import db
 
 
-app = create_app(blueprints=blueprints)
+_HERE = os.path.dirname(__file__)
+os.environ['TESTDIR'] = os.path.join(_HERE, 'tests')
+_SETTINGS = os.path.join(_HERE, 'settings.ini')
+app = create_app(blueprints=blueprints, settings=_SETTINGS)
+
+
+with open(app.config['pub_key']) as f:
+    app.config['pub_key'] = f.read()
+
+
+@app.before_request
+def before_req():
+    authenticate(app, request)
+
+
+def _400(desc):
+    exc = HTTPException()
+    exc.code = 400
+    exc.description = desc
+    return error_handling(exc)
+
+
+def authenticate(app, request):
+    key = request.headers.get('Authorization')
+    if key is None:
+        return abort(401)
+
+    key = key.split(' ')
+    if len(key) != 2:
+        return abort(401)
+
+    if key[0].lower() != 'bearer':
+        return abort(401)
+
+    pub_key = app.config['pub_key']
+    try:
+        token = key[1]
+        token = jwt.decode(token, pub_key)
+    except Exception as e:
+        return abort(401)
+
+    # we have the token.
 
 
 if __name__ == '__main__':
--- dataservice/tests/test_views.py
+++ dataservice/tests/test_views.py
@@ -1,12 +1,25 @@
+import os
 import unittest
+import jwt
 from dataservice.app import app
 from flask_webtest import TestApp as _TestApp
+
+
+_HERE = os.path.dirname(__file__)
+with open(os.path.join(_HERE, 'privkey.pem')) as f:
+    _KEY = f.read()
+
+
+def create_token(data):
+    return jwt.encode(data, _KEY, algorithm='RS512')
 
 
 class TestViews(unittest.TestCase):
     def setUp(self):
         self.app = _TestApp(app)
+        self.token = create_token({}).decode('ascii')
+        self.headers = {'Authorization': 'Bearer ' + self.token}
 
     def test_one(self):
-        resp = self.app.get('/')
+        resp = self.app.get('/', headers=self.headers)
         self.assertEqual(resp.status_code, 200)
